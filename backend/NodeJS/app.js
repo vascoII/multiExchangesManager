5ad91a187e9be135016087a26b8ff94f716186ba
VascoII <fabriceds@yahoo.fr>
2022-01-02 15:00:14 +0100

    Update app.js

--- backend/NodeJS/app.js
+++ backend/NodeJS/app.js
@@ -11,7 +11,7 @@ const mongodbAccess = require('./config/database');
 
 const app = express();
 
-mongoose.connect('mongodb+srv://' + mongodbAccess.USER + ':' + mongodbAccess.PASSWORD + '@cluster0.b4l1p.mongodb.net/sample_thing?retryWrites=true&w=majority',
+mongoose.connect('mongodb+srv://' + mongodbAccess.USER + ':' + mongodbAccess.PASSWORD + '@' + mongodbAccess.CLUSTER + '/sample_thing?retryWrites=true&w=majority',
     { useNewUrlParser: true,
         useUnifiedTopology: true })
     .then(() => console.log('Connexion à MongoDB réussie !'))
